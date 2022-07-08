8918517875012e59fb8d20de772bec6233848359
Simone Fortunato <frtsmn94@gmail.com>
2022-07-08 16:41:24 +0200

    inizio parte JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const immagini = [ "img/01.jpg",
-                   "img/01.jpg",
-                   "img/01.jpg",
-                   "img/01.jpg",
-                   "img/01.jpg",
+                   "img/02.jpg",
+                   "img/03.jpg",
+                   "img/04.jpg",
+                   "img/05.jpg",
                 ];
+
+let img = document.querySelector (".img");
+let imgarray = document.querySelectorAll('.img');
+let imgselez="";
+
+for(i = 0; i< immagini.length; i++){
+    img.innerHTML +=  ` <div class="img">
+                        <img class="img" src="${immagini[i]}">
+                        </div> `;
+}
+
+imgarray[imgselez].classList.Add(".show")
+let up = document.querySelector("up");
+let down = document.querySelector("down");
